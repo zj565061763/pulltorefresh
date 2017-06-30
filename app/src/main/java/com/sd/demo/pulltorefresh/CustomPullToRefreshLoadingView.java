ec6d7e877107981d75d5b61b6086cf3cd6b435eb
zjun <565061763@qq.com>
2017-06-30 13:42:50 +0800

    测试自定义loadingview

--- app/src/main/java/com/sd/demo/pulltorefresh/CustomPullToRefreshLoadingView.java
+++ app/src/main/java/com/sd/demo/pulltorefresh/CustomPullToRefreshLoadingView.java
@@ -5,43 +5,30 @@ import android.support.annotation.AttrRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.view.LayoutInflater;
-import android.widget.ImageView;
 
 import com.fanwe.library.pulltorefresh.ISDPullToRefreshView;
-import com.fanwe.library.pulltorefresh.SDPullToRefreshLoadingView;
+import com.fanwe.library.pulltorefresh.SDSimpleImageLoadingView;
 import com.fanwe.library.utils.SDViewUtil;
 
 /**
  * Created by Administrator on 2017/6/30.
  */
 
-public class CustomPullToRefreshLoadingView extends SDPullToRefreshLoadingView
+public class CustomPullToRefreshLoadingView extends SDSimpleImageLoadingView
 {
     public CustomPullToRefreshLoadingView(@NonNull Context context)
     {
         super(context);
-        init();
     }
 
     public CustomPullToRefreshLoadingView(@NonNull Context context, @Nullable AttributeSet attrs)
     {
         super(context, attrs);
-        init();
     }
 
     public CustomPullToRefreshLoadingView(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr)
     {
         super(context, attrs, defStyleAttr);
-        init();
-    }
-
-    private ImageView iv_image;
-
-    private void init()
-    {
-        LayoutInflater.from(getContext()).inflate(R.layout.view_custom_loading, this, true);
-        iv_image = (ImageView) findViewById(R.id.iv_image);
     }
 
     @Override
@@ -51,14 +38,14 @@ public class CustomPullToRefreshLoadingView extends SDPullToRefreshLoadingView
         {
             case RESET:
             case PULL_TO_REFRESH:
-                iv_image.setImageResource(R.drawable.ic_pull_refresh_normal);
+                getImageView().setImageResource(R.drawable.ic_pull_refresh_normal);
                 break;
             case RELEASE_TO_REFRESH:
-                iv_image.setImageResource(R.drawable.ic_pull_refresh_ready);
+                getImageView().setImageResource(R.drawable.ic_pull_refresh_ready);
                 break;
             case REFRESHING:
-                iv_image.setImageResource(R.drawable.ic_pull_refresh_refreshing);
-                SDViewUtil.startAnimationDrawable(iv_image.getDrawable());
+                getImageView().setImageResource(R.drawable.ic_pull_refresh_refreshing);
+                SDViewUtil.startAnimationDrawable(getImageView().getDrawable());
                 break;
         }
     }
