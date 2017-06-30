a90afc31bff478ef4e63501eb20e44aae4ff9fda
zjun <565061763@qq.com>
2017-06-30 13:42:28 +0800

    新增SDSimpleImageLoadingView

---
+++ libPullToRefresh/src/main/java/com/fanwe/library/pulltorefresh/SDSimpleImageLoadingView.java
@@ -0,0 +1,47 @@
+package com.fanwe.library.pulltorefresh;
+
+import android.content.Context;
+import android.support.annotation.AttrRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+
+/**
+ * Created by Administrator on 2017/6/30.
+ */
+
+public abstract class SDSimpleImageLoadingView extends SDPullToRefreshLoadingView
+{
+    public SDSimpleImageLoadingView(@NonNull Context context)
+    {
+        super(context);
+        init();
+    }
+
+    public SDSimpleImageLoadingView(@NonNull Context context, @Nullable AttributeSet attrs)
+    {
+        super(context, attrs);
+        init();
+    }
+
+    public SDSimpleImageLoadingView(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr)
+    {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private ImageView iv_image;
+
+    private void init()
+    {
+        LayoutInflater.from(getContext()).inflate(R.layout.view_simple_image_loading, this, true);
+        iv_image = (ImageView) findViewById(R.id.iv_image);
+    }
+
+    public ImageView getImageView()
+    {
+        return iv_image;
+    }
+}
