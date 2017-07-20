25f2c5eb674cd4ae064008c91e27f1087c6b07dc
zjun <565061763@qq.com>
2017-07-20 13:48:03 +0800

    修改demo

--- app/src/main/java/com/sd/demo/pulltorefresh/activity/ButtonActivity.java
+++ app/src/main/java/com/sd/demo/pulltorefresh/activity/ButtonActivity.java
@@ -1,36 +1,53 @@
 package com.sd.demo.pulltorefresh.activity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import com.fanwe.library.activity.SDBaseActivity;
+import com.fanwe.library.pulltorefresh.ISDPullToRefreshView;
 import com.fanwe.library.pulltorefresh.SDPullToRefreshView;
+import com.fanwe.library.utils.SDToast;
 import com.sd.demo.pulltorefresh.R;
 
 public class ButtonActivity extends SDBaseActivity
 {
     private SDPullToRefreshView view_pull;
-    private Button btn_stop_refresh;
 
     @Override
     protected void init(Bundle savedInstanceState)
     {
         setContentView(R.layout.activity_button);
         view_pull = (SDPullToRefreshView) findViewById(R.id.view_pull);
-        btn_stop_refresh = (Button) findViewById(R.id.btn_stop_refresh);
 
-        btn_stop_refresh.setOnClickListener(new View.OnClickListener()
+        view_pull.setDebug(true);
+        view_pull.setOnRefreshCallback(new ISDPullToRefreshView.OnRefreshCallback()
         {
             @Override
-            public void onClick(View v)
+            public void onRefreshingFromHeader(SDPullToRefreshView view)
+            {
+                SDToast.showToast("头部刷新");
+                stopRefreshingDelayed(2000);
+            }
+
+            @Override
+            public void onRefreshingFromFooter(SDPullToRefreshView view)
+            {
+                SDToast.showToast("尾部刷新");
+                stopRefreshingDelayed(2000);
+            }
+        });
+        view_pull.startRefreshingFromHeader();
+    }
+
+    private void stopRefreshingDelayed(long delay)
+    {
+        view_pull.postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
             {
                 view_pull.stopRefreshing();
             }
-        });
-
-        view_pull.setDebug(true);
-        view_pull.startRefreshingFromHeader();
+        }, delay);
     }
 
 }
