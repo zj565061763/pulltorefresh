7235ff1d1d90609211422b181203ea71f48faa54
zjun <565061763@qq.com>
2017-07-25 17:15:47 +0800

    修改demo

--- app/src/main/java/com/sd/demo/pulltorefresh/activity/ButtonActivity.java
+++ app/src/main/java/com/sd/demo/pulltorefresh/activity/ButtonActivity.java
@@ -13,16 +13,46 @@ import com.sd.demo.pulltorefresh.R;
 public class ButtonActivity extends SDBaseActivity
 {
     private SDPullToRefreshView view_pull;
-    private Button btn;
+    private Button btn_overlay, btn_stop, btn_stop_success, btn_stop_failure;
 
     @Override
     protected void init(Bundle savedInstanceState)
     {
         setContentView(R.layout.activity_button);
         view_pull = (SDPullToRefreshView) findViewById(R.id.view_pull);
-        btn = (Button) findViewById(R.id.btn);
+        btn_overlay = (Button) findViewById(R.id.btn_overlay);
+        btn_stop = (Button) findViewById(R.id.btn_stop);
+        btn_stop_success = (Button) findViewById(R.id.btn_stop_success);
+        btn_stop_failure = (Button) findViewById(R.id.btn_stop_failure);
 
-        btn.setOnClickListener(new View.OnClickListener()
+        btn_stop.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //停止刷新
+                view_pull.stopRefreshing();
+            }
+        });
+        btn_stop_success.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //停止刷新->成功
+                view_pull.stopRefreshingWithResult(true);
+            }
+        });
+        btn_stop_failure.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //停止刷新->失败
+                view_pull.stopRefreshingWithResult(false);
+            }
+        });
+        btn_overlay.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
@@ -38,34 +68,19 @@ public class ButtonActivity extends SDBaseActivity
             @Override
             public void onRefreshingFromHeader(SDPullToRefreshView view)
             {
-                stopRefreshingDelayed(1000);
             }
 
             @Override
             public void onRefreshingFromFooter(SDPullToRefreshView view)
             {
-                stopRefreshingDelayed(1000);
             }
         });
         updateBtnMode();
     }
 
-    private void stopRefreshingDelayed(long delay)
-    {
-        view_pull.postDelayed(new Runnable()
-        {
-            @Override
-            public void run()
-            {
-                changeViewHeight(btn, 100);
-                view_pull.stopRefreshingWithResult(true);
-            }
-        }, delay);
-    }
-
     private void updateBtnMode()
     {
-        btn.setText(view_pull.isOverLayMode() ? "覆盖模式" : "拖拽模式");
+        btn_overlay.setText(view_pull.isOverLayMode() ? "覆盖模式" : "拖拽模式");
     }
 
     private void changeViewHeight(View view, int changeHeight)
