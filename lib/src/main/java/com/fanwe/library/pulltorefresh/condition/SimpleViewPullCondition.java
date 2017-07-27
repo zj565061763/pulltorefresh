bd5997058a5330f52c4f0f0919634fcf83492a6e
zjun <565061763@qq.com>
2017-07-27 14:24:29 +0800

    新增SimpleViewPullCondition简单的拖动条件实现类

---
+++ lib/src/main/java/com/fanwe/library/pulltorefresh/condition/SimpleViewPullCondition.java
@@ -0,0 +1,56 @@
+package com.fanwe.library.pulltorefresh.condition;
+
+import android.support.v4.view.ViewCompat;
+import android.view.View;
+
+import com.fanwe.library.pulltorefresh.ISDPullToRefreshView;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Created by Administrator on 2017/7/27.
+ */
+public class SimpleViewPullCondition implements ISDPullToRefreshView.IPullCondition
+{
+    private WeakReference<View> mView;
+
+    public SimpleViewPullCondition(View view)
+    {
+        mView = new WeakReference<>(view);
+    }
+
+    public View getView()
+    {
+        if (mView != null)
+        {
+            return mView.get();
+        } else
+        {
+            return null;
+        }
+    }
+
+    @Override
+    public boolean canPullFromHeader()
+    {
+        if (getView() == null)
+        {
+            return true;
+        } else
+        {
+            return !ViewCompat.canScrollVertically(getView(), -1);
+        }
+    }
+
+    @Override
+    public boolean canPullFromFooter()
+    {
+        if (getView() == null)
+        {
+            return true;
+        } else
+        {
+            return !ViewCompat.canScrollVertically(getView(), 1);
+        }
+    }
+}
