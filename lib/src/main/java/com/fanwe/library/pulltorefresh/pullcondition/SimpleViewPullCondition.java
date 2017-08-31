a8d3376acaacfd292d77b2a19d1ef324848c1215
zjun <565061763@qq.com>
2017-08-31 17:02:18 +0800

    开源协议

--- lib/src/main/java/com/fanwe/library/pulltorefresh/pullcondition/SimpleViewPullCondition.java
+++ lib/src/main/java/com/fanwe/library/pulltorefresh/pullcondition/SimpleViewPullCondition.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2017 zhengjun, fanwe (http://www.fanwe.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.fanwe.library.pulltorefresh.pullcondition;
 
 import android.support.v4.view.ViewCompat;
@@ -7,9 +22,6 @@ import com.fanwe.library.pulltorefresh.ISDPullToRefreshView;
 
 import java.lang.ref.WeakReference;
 
-/**
- * Created by Administrator on 2017/7/27.
- */
 public class SimpleViewPullCondition implements ISDPullToRefreshView.IPullCondition
 {
     private WeakReference<View> mView;
